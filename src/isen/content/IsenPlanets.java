3f29d4acbb736507d4d8abb76ce0e46ecf02e5ed
CamoGekko <142858559+CamoGekko@users.noreply.github.com>
2024-02-07 18:50:33 +0000

    Fix

--- src/isen/content/IsenPlanets.java
+++ src/isen/content/IsenPlanets.java
@@ -45,7 +45,7 @@ public class IsenPlanets{
             clearSectorOnLose = true;
             defaultCore = Blocks.coreShard;
             iconColor = Color.valueOf("8c826b");
-            hiddenItems.addAll(Items.serpuloItems).removeAll(Items.erekirItems);
+            hiddenItems.addAll(Items.erekirItems).removeAll(Items.serpuloItems);
             enemyBuildSpeedMultiplier = 0.4f;
 
             //TODO disallowed for now
@@ -57,11 +57,11 @@ public class IsenPlanets{
             defaultAttributes.set(Attribute.heat, 0.8f);
 
             ruleSetter = r -> {
-                r.waveTeam = Team.malis;
+                r.waveTeam = Team.sharded;
                 r.placeRangeCheck = false;
-                r.showSpawns = true;
+                r.showSpawns = false;
                 r.fog = true;
-                r.staticFog = true;
+                r.staticFog = false;
                 r.lighting = false;
                 r.coreDestroyClear = true;
                 r.onlyDepositCore = true;
@@ -70,45 +70,4 @@ public class IsenPlanets{
             unlockedOnLand.add(Blocks.coreShard);
         }};
     }
-
-    private static Planet makeAsteroid(String name, Planet parent, Block base, Block tint, float tintThresh, int pieces, float scale, Cons<AsteroidGenerator> cgen){
-        return new Planet(name, parent, 0.12f){{
-            hasAtmosphere = false;
-            updateLighting = false;
-            sectors.add(new Sector(this, Ptile.empty));
-            camRadius = 0.68f * scale;
-            minZoom = 0.6f;
-            drawOrbit = false;
-            accessible = false;
-            clipRadius = 2f;
-            defaultEnv = Env.space;
-            icon = "commandRally";
-            generator = new AsteroidGenerator();
-            cgen.get((AsteroidGenerator)generator);
-
-            meshLoader = () -> {
-                iconColor = tint.mapColor;
-                Color tinted = tint.mapColor.cpy().a(1f - tint.mapColor.a);
-                Seq<GenericMesh> meshes = new Seq<>();
-                Color color = base.mapColor;
-                Rand rand = new Rand(id + 2);
-
-                meshes.add(new NoiseMesh(
-                    this, 0, 2, radius, 2, 0.55f, 0.45f, 14f,
-                    color, tinted, 3, 0.6f, 0.38f, tintThresh
-                ));
-
-                for(int j = 0; j < pieces; j++){
-                    meshes.add(new MatMesh(
-                        new NoiseMesh(this, j + 1, 1, 0.022f + rand.random(0.039f) * scale, 2, 0.6f, 0.38f, 20f,
-                        color, tinted, 3, 0.6f, 0.38f, tintThresh),
-                        new Mat3D().setToTranslation(Tmp.v31.setToRandomDirection(rand).setLength(rand.random(0.44f, 1.4f) * scale)))
-                    );
-                }
-
-                return new MultiMesh(meshes.toArray(GenericMesh.class));
-            };
-        }};
-    }
-
 }
